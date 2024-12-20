9f24488613855b57e7ec7039d15fdb9b74b9de60
Shira Elovici <shiraelovici@gmail.com>
2024-12-20 16:46:45 +0200

    did the rest of thier dosumentattion

--- Knight.h
+++ Knight.h
@@ -7,18 +7,18 @@ public:
 
 private:
 	/*
-	name:
-	input:
-	output:
-	description:
+	name: move
+	input: a refrence to a place
+	output: none
+	description: this function moves the piece to the given dest
 	*/
 	virtual void move(const Place& dest) override;
 
 	/*
-	name:
-	input:
-	output:
-	description:
+	name: isValidMove
+	input: a refrence to a place
+	output: a num that represents the validility
+	description: this function tells if the move is valid or not and why
 	*/
 	virtual int isValidMove(const Place& dest) const override;
 

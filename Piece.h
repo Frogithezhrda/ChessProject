a11c276b90357e7e5edf6747fdc77835b60731c6
Shira Elovici <shiraelovici@gmail.com>
2024-12-20 13:37:48 +0200

    did documentry for piece

--- Piece.h
+++ Piece.h
@@ -2,59 +2,55 @@
 #include <iostream>
 #include "Place.h"
 
-class Piece
+class Piece//represents a piece on the board
 {
 public:
 	/*
-	name:
-	input:
-	output:
-	description:
+	name: CONSTRUCTOR
+	input: pieceColor - the color of the piece(B/W). type
 	*/
 	Piece(const char pieceColor, const std::string& type, const Place& firstPlace);
 
 	/*
-	name:
-	input:
-	output:
-	description:
+	name: CONSTRUCTOR
+	input: a refrense to a string that represents the location on the board
 	*/
 	~Piece();
 
 	/*
-	name:
-	input:
-	output:
-	description:
+	name: move
+	input: a refrence to a place 
+	output: none
+	description: this function moves the piece to the given dest
 	*/
 	virtual void move(const Place& dest) = 0;
 
 	/*
-	name:
-	input:
-	output:
-	description:
+	name: isValidMove
+	input: a refrence to a place
+	output: a num that represents the validility
+	description: this function tells if the move is valid or not and why
 	*/
 	virtual int isValidMove(const Place& dest) const = 0;
 
 	/*
-	name:
-	input:
-	output:
-	description:
+	name: getPieceColor
+	input: none
+	output:a char that represents the clor of the piece (B/W)
+	description: this function returns the color of the piece
 	*/
 	char getPieceColor() const;
 
 	/*
-	name:
-	input:
-	output:
-	description:
+	name: getType
+	input: none
+	output: a string that represents a type
+	description: this fyunction returns the type of the piece
 	*/
 	std::string getType() const;
 
 private:
-	char _pieceColor;
-	std::string _type;
-	Place _currentPlace;
+	char _pieceColor;//the color of the piece
+	std::string _type;//the type of piece
+	Place _currentPlace;//the current location of the piece
 };

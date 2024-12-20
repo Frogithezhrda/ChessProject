bc2bf28c7c5401dcce0825d98fe27d6ecf60ab80
Shira Elovici <shiraelovici@gmail.com>
2024-12-20 19:42:00 +0200

    started king

---
+++ King.cpp
@@ -0,0 +1,25 @@
+#include "King.h"
+
+
+
+/*
+	name: move
+	input: a refrence to a place
+	output: none
+	description: this function moves the piece to the given dest
+*/
+void King::move(const Place& dest) 
+{
+
+}
+
+/*
+name: isValidMove
+input: a refrence to a place
+output: a num that represents the validility
+description: this function tells if the move is valid or not and why
+*/
+int King::isValidMove(const Place& dest) const
+{
+
+}

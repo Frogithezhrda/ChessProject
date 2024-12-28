5ef0e3821c017394eef99f1ad443e91340a43a2a
Shira Elovici <shiraelovici@gmail.com>
2024-12-28 15:48:50 +0200

    documentatioonnnnnnnnnnnnnnnnnnnn

--- Board.cpp
+++ Board.cpp
@@ -40,33 +40,33 @@ Piece* Board::getPiece(const std::string& pieceLocation) const
 	int row = pieceLocation[ROW_INDEX] - '1'; 
 	int col = pieceLocation[COLUM_INDEX] - 'a'; 
 
-	if (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE) 
+	if (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE) //if row and col are valid...
 	{
 		place = &_board[row][col];
 	}
-				pieceLetter = place->getCurrentPiece();
+				pieceLetter = place->getCurrentPiece();//the typemof piece in the givven location
 				switch (std::tolower(pieceLetter))
 				{
-					case PawnName:
+					case PawnName://if the piece is a pawn
 						piece = new Pawn(std::islower(pieceLetter) ? WHITE : BLACK, *place);
 						break;
-					case KnightName:
+					case KnightName://if the piece is a knight
 						piece = new Knight(std::islower(pieceLetter) ? WHITE : BLACK, *place);
 						break;
-					case RookName:
+					case RookName://if the piece is a rook
 						piece = new Rook(std::islower(pieceLetter) ? WHITE : BLACK, *place);
 						break;
-					case KingName:
+					case KingName://if the piece is a king
 						piece = new King(std::islower(pieceLetter) ? WHITE : BLACK, *place);
 						break;
-					case QueenName:
+					case QueenName://if the piece is a queen
 						piece = new Queen(std::islower(pieceLetter) ? WHITE : BLACK, *place);
 						break;
-					case BishopName:
+					case BishopName://if the piece is a bishop
 						piece = new Bishop(std::islower(pieceLetter) ? WHITE : BLACK, *place);
 						break;
 				}
-				return piece;
+				return piece;//returning tthe piece :)
 }
 void Board::printBoard() const
 {
@@ -91,30 +91,31 @@ void Board::setBoard(const std::string& src, const Place& dest)
 	char movingPieceChar = ' '; 
 	bool srcFound = false;
 
-	for (i = 0; i < BOARD_SIZE; i++)
+	for (i = 0; i < BOARD_SIZE; i++)//going alll over the board
 	{
 		for (j = 0; j < BOARD_SIZE; j++)
 		{
-			if (src == this->_board[i][j].getLocation())
+			if (src == this->_board[i][j].getLocation())//if we found the src
 			{
 				movingPieceChar = this->_board[i][j].getCurrentPiece();
-
+				//there is nothing in rcc anymore cuz we moving it to dest
 				this->_board[i][j].setPiece('#'); 
 				this->_board[i][j].deactivePiece();
 
-				srcFound = true;
+				srcFound = true;//the src was foundd
 				break;
 			}
 		}
 		if (srcFound) break;
 	}
 
-	for (i = 0; i < BOARD_SIZE; i++)
+	for (i = 0; i < BOARD_SIZE; i++)//thrugh the whoolleeee board
 	{
 		for (j = 0; j < BOARD_SIZE; j++)
 		{
-			if (dest.getLocation() == this->_board[i][j].getLocation())
+			if (dest.getLocation() == this->_board[i][j].getLocation())//if we found the dest
 			{
+				//setting the piece to dest
 				this->_board[i][j] = dest;
 				this->_board[i][j].setPiece(movingPieceChar);
 				this->_board[i][j].activePiece();
@@ -135,11 +136,11 @@ bool Board::isValidPosition(const std::string& position) const
 
 
 
-	if ((row >= FIRST_COLUM_INDEX && row <= LAST_COLUM_INDEX) && (column >= FIRST_ROW_INDEX && column <= LAST_ROW_INDEX))
+	if ((row >= FIRST_COLUM_INDEX && row <= LAST_COLUM_INDEX) && (column >= FIRST_ROW_INDEX && column <= LAST_ROW_INDEX))//if the row and colum is in the valid range, then return true
 	{
 		return true; 
 	}
-
+	//otherwise false
 	return false; 
 }
 
@@ -149,7 +150,7 @@ void Board::setPieceAtBoard(const std::string& dest, Piece* piece)
 	int row = dest[ROW_INDEX] - '1';
 	int col = dest[COLUM_INDEX] - 'a';
 
-	if (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE) 
+	if (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE) //if the row and col are valid, then inits the piece at given dest
 	{
 		place = &_board[row][col];
 		place->setPiece(piece->getType());

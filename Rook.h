b99dd6ca54a3dffc04310d1a20c55a1415c0113e
Shira Elovici <shiraelovici@gmail.com>
2024-12-28 12:14:53 +0200

    moreeee fixes

--- Rook.h
+++ Rook.h
@@ -16,14 +16,15 @@ public:
 	name: DESTRUCTOR
 	*/
 	virtual ~Rook();
-	/*
-	name: move
-	input: a refrence to a place
-	output: none
-	description: this function moves the piece to the given dest
-	*/
 
+	/*
+	name: isclearpath
+	input: a refrence to a string(dest), a refrense to a place(src, and a p
+	output: a num that represents the validility
+	description: this function tells if the path between src to dest is clear or not
+	*/
 	bool isClearPath(const Place& dest, const Place& src, const Board* board) const;
+
 	/*
 	name: isValidMove
 	input: a refrence to a place(dest), a pointer to the board, a pointer to a player, and a pointer to the opp player.
